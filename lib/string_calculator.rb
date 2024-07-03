5b3dcb1a4fa6b92459f9c5beed15c88e87fb90fa
salvi.mohan4 <salvi.mohan4@gmail.com>
2024-07-03 15:43:10 +0530

    cover scenerio for new line between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,11 +5,12 @@ class StringCalculator
     def add(string_number = '')
       return 0 if string_number.empty?
 
-      numbers = string_number.split(',').map(&:to_i)
-      negatives = numbers.select(&:negative?)
+      numbers = string_number.split(',')
+      negatives = numbers.map(&:to_i).select(&:negative?)
       raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
 
-      numbers.sum
+      numbers = string_number.split(/,|\n/)
+      numbers.map(&:to_i).sum
     end
   end
 end
