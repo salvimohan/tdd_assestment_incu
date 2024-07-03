744ea356ed2713366ca8575b643475f6bcf94f9c
salvi.mohan4 <salvi.mohan4@gmail.com>
2024-07-03 15:26:25 +0530

    cover scenerio for no argument

---
+++ lib/string_calculator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class StringCalculator
+  class << self
+    def add(string_number = nil)
+      return 0 if string_number.nil?
+    end
+  end
+end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,3 +1,5 @@
+require_relative './../lib/string_calculator'
+
 RSpec.describe StringCalculator do
   describe '.add' do
     it 'return 0 for no argument' do
